f5bbe25c9495deb8f96a374032a0c6664b2d4119
Hal Wang <halhaoranwang@gmail.com>
2019-12-27 22:30:18 +0800

    we added the CanvasView

---
+++ Whiteboard/CanvasView.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasView.swift
+//  Whiteboard
+//
+//  Created by Halwong on 2019/12/27.
+//  Copyright © 2019 halhaoranwang. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    override func draw(_ rect: CGRect) {
+
+    }
+
+}
