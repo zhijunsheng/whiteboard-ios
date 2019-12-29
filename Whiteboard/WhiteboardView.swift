dcda0ca982001854a2dc5d082dbc24da1a6183e9
daobao <lucashongyuanzhang@gmail.com>
2019-12-29 09:20:15 +0800

    we can draw on the iPad

--- Whiteboard/WhiteboardView.swift
+++ Whiteboard/WhiteboardView.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class WhiteboardView: UIView {
 
+    var points: [CGPoint] = []
     
     
     override func draw(_ rect: CGRect) {
@@ -9,7 +10,29 @@ class WhiteboardView: UIView {
         let Aa = UIBezierPath()
         Aa.move(to: CGPoint(x: 3, y: 13))
         Aa.addLine(to: CGPoint(x: 43, y: 143))
+        
+        for point in points {
+            Aa.addLine(to: point)
+        }
+        
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         Aa.stroke()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let touchLocation = touch.location(in: self)
+ //       print("\(touchLocation.x), \(touchLocation.y)")
+        
+        points.append(touchLocation)
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
     }
 }
